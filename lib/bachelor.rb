aa0b844b5dc0817bf3e9aacf66f305a746a4ec7d
gwatson86 <gwatson86@gmail.com>
2018-08-19 18:49:47 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+
+  data[season].each do |bachelorette|
+    if bachelorette["status"] == "Winner"
+      return bachelorette["name"].split[0]
+    end
+  end
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
+  data.each do |season, bachelorettes|
+    bachelorettes.each do |bachelorette|
+      if bachelorette["occupation"] == occupation
+        return bachelorette["name"]
+      end
+    end
+  end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+
+  i = 0
+
+  data.each do |season, bachelorettes|
+    bachelorettes.each do |bachelorette|
+      if bachelorette["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+
+  i
 end
 
 def get_occupation(data, hometown)
-  # code here
+
+  data.each do |season, bachelorettes|
+    bachelorettes.each do |bachelorette|
+      if bachelorette["hometown"] == hometown
+        return bachelorette["occupation"]
+      end
+    end
+  end
+
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+
+  age_sum = 0
+  how_many_contestants = 0
+
+  data[season].each do |bachelorette|
+    age_sum += bachelorette["age"].to_f
+    how_many_contestants += 1
+  end
+
+  (age_sum/how_many_contestants).round
 end
